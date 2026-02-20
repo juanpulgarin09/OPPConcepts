581d6ac01b5f6862aaacb22bc1ce332f05461fe8
juanpulgarin09 <juanpulgarin.09az@gmail.com>
2026-02-19 21:35:53 -0500

    Add BaseCommisinEmployee class

---
+++ OPPConcepts/OPPConcepts.Backend/SalaryEmploye.cs
@@ -0,0 +1,35 @@
+﻿namespace OPPConcepts.Backend;
+
+public class SalaryEmployee : Employee
+{
+    // Fields
+    private decimal _salary;
+
+    // Constructors
+    public SalaryEmployee(int id, string firstName, string lastName, bool isActive, Date bornDate, Date hireDate, decimal salary) :
+        base(id, firstName, lastName, isActive, bornDate, hireDate)
+    {
+        Salary = salary;
+    }
+
+    // Properties
+    public decimal Salary
+    {
+        get => _salary;
+        set => _salary = ValidateSalary(value);
+    }
+
+    // Methods
+    public override decimal GetValueToPay() => Salary;
+
+    public override string ToString() => base.ToString();
+
+    private decimal ValidateSalary(decimal salary)
+    {
+        if (salary < 2000000)
+        {
+            throw new ArgumentOutOfRangeException(nameof(salary), "Salary must be greater than or equal to $2,000,000.");
+        }
+        return salary;
+    }
+}
--- OPPConcepts/OPPConcepts.Frontend/Program.cs
+++ OPPConcepts/OPPConcepts.Frontend/Program.cs
@@ -2,16 +2,35 @@
 
 try
 {
-    var date1 = new Date();
-    var date2 = new Date(2026, 2, 28);
-    var date3 = new Date(2012, 11, 30);
+    var employees = new List<Employee>();
+    decimal payroll = 0;
+    var employee1 = new SalaryEmployee(1010, "Maria", "Perez", true, new Date(1990, 5, 15), new Date(2020, 1, 1), 2500000);
+    employees.Add(employee1);
+    var employee2 = new SalaryEmployee(2020, "Joaquin", "Gonzalez", true, new Date(1980, 3, 5), new Date(2016, 11, 16), 10395876);
+    employees.Add(employee2);
+    var employee3 = new HourlyEmployee(3030, "Ana", "Lopez", true, new Date(1995, 8, 20), new Date(2022, 6, 1), 18000, 95);
+    employees.Add(employee3);
+    var employee4 = new HourlyEmployee(4040, "Kevin", "Rua", true, new Date(1998, 8, 20), new Date(2021, 6, 1), 10000, 112);
+    employees.Add(employee4);
+    var employee5 = new CommisionEmployee(5050, "Sofia", "Gomez", true, new Date(1992, 12, 10), new Date(2018, 3, 15), 0.03f, 600000000);
+    employees.Add(employee5);
+    var employee6 = new CommisionEmployee(6060, "Diego", "Martinez", true, new Date(1985, 7, 30), new Date(2010, 9, 1), 0.03f, 120000000);
+    employees.Add(employee6);
+    var employee7 = new BaseCommisionEmployee(7070, "Laura", "Garcia", true, new Date(1993, 11, 25), new Date(2019, 5, 20), 0.015f, 250000000, 550000);
+    employees.Add(employee7);
+    var employee8 = new BaseCommisionEmployee(8080, "Carlos", "Rodriguez", true, new Date(1988, 4, 10), new Date(2026, 2, 10), 0.015f, 0, 550000);
+    employees.Add(employee8);
 
-    Console.WriteLine(date1);
-    Console.WriteLine(date2);
-    Console.WriteLine(date3);
+    foreach (var employee in employees)
+    {
+        Console.WriteLine(new string('-', 47));
+        Console.WriteLine(employee);
+        payroll += employee.GetValueToPay();
+    }
+    Console.WriteLine(new string('=', 47));
+    Console.WriteLine($"Payroll..................: {payroll,20:C2}");
 }
-catch(Exception ex)
+catch (Exception ex)
 {
     Console.WriteLine(ex.Message);
 }
-
